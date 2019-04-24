aec9b0453ecbeaa5565d7638ed00212bac6b3459
theovit <ovdroidx@gmail.com>
2019-04-24 17:18:09 -0500

    Delete probability.js

--- probability/probability.js
+++
@@ -1,9 +0,0 @@
-function Calculate() {
-var Year = document.getElementById('userInputYear').value;
-function YearDown(a) {
-    
-    Year = parseInt(a),
-//    var loopLength = parseInt(LoopTemp.toString().length);
-    document.getElementById("OPNResult").innerHTML = Year;
-}
-}
